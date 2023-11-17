7e84fa9f0f4ad770a5b961530891a34b5ce383e0
Mauricio Valencia <mvalenciau@sura.com.co>
2023-11-16 20:58:50 -0500

    (FIX) Se comenta NODE_TLS_REJECT_UNAUTHORIZED ya que cuasa error en el despliegue

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,4 @@
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
+//process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
 import { unstable_noStore as noStore } from 'next/cache';
 import { sql } from '@vercel/postgres';
 import {
